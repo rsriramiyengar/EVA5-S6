37a13981186c04ed9ce149aa07581fe19771ed87
Sriram Iyengar <67703785+rsriramiyengar@users.noreply.github.com>
2020-09-17 10:19:48 +0530

    Update S6_train_test_function.py

--- S6_train_test_function.py
+++ S6_train_test_function.py
@@ -28,7 +28,7 @@ def train(model, device, train_loader, optimizer, epoch, l1_lambda=None):
     y_pred = model(data)
 
     # Calculate loss
-    loss = F.nll_loss(y_pred, target)
+    loss = nnF.nll_loss(y_pred, target)
 
     # l1 regularization
     if l1_lambda:
@@ -60,7 +60,7 @@ def test(model, device, test_loader):
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += nnF.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
