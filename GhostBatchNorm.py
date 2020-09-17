4bbf6fe944a70ac7f6cc50e4d75d45595aea03c1
Sriram Iyengar <67703785+rsriramiyengar@users.noreply.github.com>
2020-09-17 11:07:35 +0530

    Update GhostBatchNorm.py

--- GhostBatchNorm.py
+++ GhostBatchNorm.py
@@ -8,6 +8,15 @@ import torch.optim as optim
 #and we are using datasets and transforms from this library 
 from torchvision import datasets, transforms
 
+import torch.nn as nn
+class BatchNorm(nn.BatchNorm2d):
+    def __init__(self, num_features, eps=1e-05, momentum=0.1, weight=True, bias=True):
+        super().__init__(num_features, eps=eps, momentum=momentum)
+        self.weight.data.fill_(1.0)
+        self.bias.data.fill_(0.0)
+        self.weight.requires_grad = weight
+        self.bias.requires_grad = bias
+        
 class GhostBatchNorm(BatchNorm):
     def __init__(self, num_features, num_splits, **kw):
         super().__init__(num_features, **kw)
